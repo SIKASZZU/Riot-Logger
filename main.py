7a96f05b3f08d431b5d1ff3fca22d65bde47b072
SIKASZZU <soots.sigvrid@gmail.com>
2025-02-06 15:41:33 +0200

    clicks are now respective to window location

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-
 import subprocess
 import pygetwindow as gw
 import pyautogui
 import tkinter as tk
 import time
 
-from acc_details import acc_details 
-# acc_details = {'Account_Name': ('Username', 'Passcode'),}
-
+from acc_details import acc_details  # Ensure acc_details is a dictionary with account info
 
 ################# open riot client #################
 subprocess.Popen(r"C:\Riot Games\Riot Client\RiotClientServices.exe")
 print('Opening Riot Client\n')
-
 
 ################# check if client opened #################
 while True:
@@ -22,11 +18,9 @@ while True:
     for title in open_windows:
         if title == 'Riot Client':
             client_opened = True
-        
     if client_opened == True:  break
 
 print('Riot Client is open\n')
-
 
 ################# Window, select account #################
 
@@ -46,12 +40,12 @@ label.pack(pady=10)
 for key in acc_details.keys():
     button = tk.Button(root, text=key, command=lambda key=key: on_button_click(key))
     button.pack(pady=5)
+
 root.mainloop()
 
 print('ACCOUNT', account_selected, '\n')
 username = acc_details[account_selected][0]
 passcode = acc_details[account_selected][1]
-
 
 ################# Active window Riot Client #################
 riot_window = gw.getWindowsWithTitle('Riot Client')[0]  # Find the first window with the title 'Riot Client'
@@ -60,15 +54,23 @@ riot_window.activate()  # Activate the window to bring it to the foreground
 time.sleep(0.5)  # Optionally, give some time for the window to become active
 
 ################# Log into the account #################
-user_xy         = (500, 350)
-pass_xy         = (500, 450)
-login_button_xy = (500, 800)
+user_xy         = (200, 275)
+pass_xy         = (200, 320)
+login_button_xy = (200, 700)
+
+# Get the window's position and size (left, top, width, height)
+left, top, width, height = riot_window.left, riot_window.top, riot_window.width, riot_window.height
+
+# Adjust the click coordinates relative to the Riot Client window
+adjusted_user_xy = (left + user_xy[0], top + user_xy[1])
+adjusted_pass_xy = (left + pass_xy[0], top + pass_xy[1])
+adjusted_login_button_xy = (left + login_button_xy[0], top + login_button_xy[1])
 
 ################# Clicking #################
-pyautogui.click(user_xy)
+pyautogui.click(adjusted_user_xy)
 pyautogui.typewrite(username)
 
-pyautogui.click(pass_xy)
+pyautogui.click(adjusted_pass_xy)
 pyautogui.typewrite(passcode)
 
-pyautogui.click(login_button_xy)
+pyautogui.click(adjusted_login_button_xy)
