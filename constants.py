7c3e0de64535d9c91311b88dcfef2231b320cddb
SIKASZZU <soots.sigvrid@gmail.com>
2025-02-06 15:35:35 +0200

    batman

---
+++ constants.py
@@ -0,0 +1,21 @@
+# Colors and styles
+BG_COLOR = "#2d2d2d"
+FG_COLOR = "#ffffff"
+BUTTON_COLOR = "#3498db"
+ENTRY_BG = "#DCDAD5"
+ENTRY_FG = "BLACK"
+TEXT_BG = "#3d3d3d"
+GRAYISH_BG = "#707070"  # You can adjust this value to a lighter or darker gray shade
+
+# Other constants
+DEFAULT_LOOP_COUNT = 1
+
+
+
+frame_background_color = '#2E2E2E'
+parent_frame_background_color = '#3A3A3A'
+text_color = '#E0E0E0'
+button_color = '#426F7D'
+button_hover_color = '#2E4D57'
+highlight_color = '#A0A4A8'
+alerts_color = '#D97A4C'
---
+++ main.py
@@ -0,0 +1,74 @@
+
+import subprocess
+import pygetwindow as gw
+import pyautogui
+import tkinter as tk
+import time
+
+from acc_details import acc_details 
+# acc_details = {'Account_Name': ('Username', 'Passcode'),}
+
+
+################# open riot client #################
+subprocess.Popen(r"C:\Riot Games\Riot Client\RiotClientServices.exe")
+print('Opening Riot Client\n')
+
+
+################# check if client opened #################
+while True:
+    client_opened = False
+    windows = gw.getAllTitles()
+    open_windows = [window for window in windows if window.strip()]
+    for title in open_windows:
+        if title == 'Riot Client':
+            client_opened = True
+        
+    if client_opened == True:  break
+
+print('Riot Client is open\n')
+
+
+################# Window, select account #################
+
+account_selected = None
+def on_button_click(key):
+    global account_selected
+    account_selected = key
+    root.destroy()
+
+root = tk.Tk()
+root.title("Account Details Keys")
+
+label = tk.Label(root, text="Choose account")
+label.pack(pady=10)
+
+# Create buttons for each account in acc_details dictionary
+for key in acc_details.keys():
+    button = tk.Button(root, text=key, command=lambda key=key: on_button_click(key))
+    button.pack(pady=5)
+root.mainloop()
+
+print('ACCOUNT', account_selected, '\n')
+username = acc_details[account_selected][0]
+passcode = acc_details[account_selected][1]
+
+
+################# Active window Riot Client #################
+riot_window = gw.getWindowsWithTitle('Riot Client')[0]  # Find the first window with the title 'Riot Client'
+riot_window.activate()  # Activate the window to bring it to the foreground
+
+time.sleep(0.5)  # Optionally, give some time for the window to become active
+
+################# Log into the account #################
+user_xy         = (500, 350)
+pass_xy         = (500, 450)
+login_button_xy = (500, 800)
+
+################# Clicking #################
+pyautogui.click(user_xy)
+pyautogui.typewrite(username)
+
+pyautogui.click(pass_xy)
+pyautogui.typewrite(passcode)
+
+pyautogui.click(login_button_xy)
