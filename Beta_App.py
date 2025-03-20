38a18f8de989d4089276e84d11e5b207761a36b9
Oljake <109909586+Oljake@users.noreply.github.com>
2025-03-20 19:35:42 +0200

    TODO: API reset func - Edit account (right click onto existing account) - Data (save to json file)

--- Beta_App.py
+++ Beta_App.py
@@ -5,50 +5,56 @@ from PyQt6.QtGui import QPixmap, QFont, QCursor
 from PyQt6.QtCore import Qt
 from PIL import Image, ImageDraw
 
-from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QVBoxLayout, QHBoxLayout, QScrollArea
+from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QVBoxLayout, QHBoxLayout, QScrollArea, QComboBox
 from PyQt6.QtGui import QCursor, QFont
 from PyQt6.QtCore import Qt, QPropertyAnimation, QSize
 
+from api import get_data
+
+from dotenv import load_dotenv
+import os
+
+from PyQt6.QtWidgets import QComboBox, QStyledItemDelegate
+from PyQt6.QtCore import Qt
+
+
 # Available ranks
-RANKS = [
-    "Iron I",
-    "Iron II",
-    "Iron III",
-    "Iron VI",
+RANKS = {
+    "Iron":         'images/iron.png',
+    "Bronze":       'images/bronze.png',
+    "Silver":       'images/silver.png',
+    "Gold":         'images/gold.png',
+    "Platinum":     'images/platinum.png',
+    "Diamond":      'images/diamond.png',
+    "Master":       'images/master.png',
+    "Grandmaster":   'images/grandmaster.png',
+    "Challenger":   'images/challenger.png'}
 
-    "Bronze I",
-    "Bronze II",
-    "Bronze III",
-    "Bronze VI",
+REGIONS = [
+    '',
+    'BR1',
+    'EUN1',
+    'EUW1',
+    'JP1',
+    'KR',
+    'LA1',
+    'LA2',
+    'ME1',
+    'NA1',
+    'OC1',
+    'RU',
+    'SG2',
+    'TR1',
+    'TW2',
+    'VN2',
+]
 
-    "Silver I",
-    "Silver II",
-    "Silver III",
-    "Silver VI",
-
-    "Gold I",
-    "Gold II",
-    "Gold III",
-    "Gold VI",
-
-    "Platinum I",
-    "Platinum II",
-    "Platinum III",
-    "Platinum VI",
-
-    "Diamond I",
-    "Diamond II",
-    "Diamond III",
-    "Diamond VI",
-
-    "Master",
-
-    "Challenger"]
 
 # User data dictionary
 users = [
-    {'riot_id': 'SikzuOnRäigeAutist', 'tagline': 'Asia', 'username': 'darkwizard', 'password': 'avadaKedavra'},
-    {'riot_id': 'asd', 'tagline': 'asd', 'username': 'asd', 'password': 'asd'}
+    {'riot_id': 'hate being sober',     'tagline': '420',   'region': 'eun1',   'username': 'test', 'password': 'test'},
+    {'riot_id': 'player3',              'tagline': '2002',  'region': 'eun1',   'username': 'test1', 'password': 'test1'},
+    {'riot_id': 'SAMICELASICE',         'tagline': 'cígoš', 'region': 'eun1',   'username': 'test2', 'password': 'test2'},
 ]
 
 
@@ -66,21 +72,39 @@ def create_rounded_image(image_path, size, radius):
     img.putalpha(mask)
 
     # Convert to QPixmap
-    img.save("temp_rounded.png")  # Temporary save for conversion
-    return QPixmap("temp_rounded.png")
+    img.save("images/temp_rounded.png")  # Temporary save for conversion
+    return QPixmap("images/temp_rounded.png")
 
 
 class AccountButton(QWidget):
-    def __init__(self, user_data, image_path, w, l, width, height, radius, parent=None):
+    def __init__(self, user_data, width, height, radius, image_path='images/default.png', parent=None):
         super().__init__(parent)
 
         self.setFixedSize(width, height)
-        winrate = 0 if (w + l) == 0 else round((w / (w + l)) * 100)
 
-        self.rank = random.choice(RANKS)
         self.account_name = f"{user_data['riot_id']} # {user_data['tagline']}"
         self.username = user_data['username']
         self.password = user_data['password']
+        self.riot_id = user_data['riot_id']
+        self.tagline = user_data['tagline']
+        self.region = user_data['region']
+
+        self.winrate = None
+        self.rank = None
+
+        api_key = os.getenv('riot_api_key')
+        if not api_key:
+            raise ValueError("API key not found. Check your .env file.")
+
+        ranked_info = get_data(self.riot_id, self.tagline, self.region, api_key)
+
+        if ranked_info:
+            (rank, self.rank), self.winrate = ranked_info
+
+            rank = rank.capitalize()
+            print(rank)
+            if rank in RANKS:
+                image_path = RANKS[rank]
 
         # Load rounded image
         self.bg_pixmap = create_rounded_image(image_path, (width, height), radius)
@@ -98,15 +122,15 @@ class AccountButton(QWidget):
         self.account_label.setGeometry(10, 15, 250, 20)
 
         # Winrate (Centered)
-        self.winrate_label = QLabel(f"{w}W/{l}L {winrate:.1f}%", self)
-        self.winrate_label.setFont(QFont("Arial", 10))
+        self.winrate_label = QLabel(self.winrate, self)
+        self.winrate_label.setFont(QFont("Arial", 8))
         self.winrate_label.setStyleSheet("color: gray; background: transparent;")
         self.winrate_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.winrate_label.setGeometry(260, 7, 140, 20)
 
         # Rank (Centered)
         self.rank_label = QLabel(self.rank, self)
-        self.rank_label.setFont(QFont("Arial", 10))
+        self.rank_label.setFont(QFont("Arial", 8))
         self.rank_label.setStyleSheet("color: gray; background: transparent;")
         self.rank_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.rank_label.setGeometry(260, 22, 140, 20)
@@ -147,9 +171,14 @@ class AccountButton(QWidget):
         self.winrate_label.setStyleSheet("color: gray; background: transparent;")
         self.rank_label.setStyleSheet("color: gray; background: transparent;")
 
-
     def on_click(self):
         print(f"Username: {self.username}, Password: {self.password}")
+
+
+class CenterDelegate(QStyledItemDelegate):
+    def paint(self, painter, option, index):
+        option.displayAlignment = Qt.AlignmentFlag.AlignCenter
+        super().paint(painter, option, index)
 
 
 class CreateAccount(QWidget):
@@ -160,11 +189,10 @@ class CreateAccount(QWidget):
         self.default_height = height  # Store default height
         self.expanded_height = height + 120  # Adjust height for entries
         self.radius = radius
-        self.minimized_image_path = "create_new.png"
-        self.expanded_image_path = "expanded_create_new.png"
+        self.minimized_image_path = "images/create_new.png"
+        self.expanded_image_path = "images/expanded_create_new.png"
 
         self.setFixedSize(width, self.default_height)
-
 
         # Load rounded image
         self.bg_pixmap = create_rounded_image(self.minimized_image_path, (width, height), 0)
@@ -204,8 +232,8 @@ class CreateAccount(QWidget):
         self.expanded_bg_label.setPixmap(QPixmap(self.expanded_bg_pixmap))
         self.expanded_bg_label.setScaledContents(True)
         self.expanded_bg_label.setGeometry(0, 0, width, self.expanded_height)
-        self.expanded_bg_label.hide()
         self.expanded_bg_label.setStyleSheet("background: transparent;")
+        self.expanded_bg_label.hide()
 
         # Input Fields (Hidden Initially)
         self.riot_id_entry = QLineEdit(self)
@@ -232,16 +260,27 @@ class CreateAccount(QWidget):
         self.password_entry.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.password_entry.hide()
 
+        # Region Selector Dropdown
+        self.combo_box = QComboBox(self)
+        self.combo_box.addItems(REGIONS)
+        self.combo_box.setItemDelegate(CenterDelegate(self.combo_box))
+        self.combo_box.setGeometry(80, 130, 80, 30)
+        self.combo_box.setEditable(True)
+        self.combo_box.lineEdit().setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.combo_box.lineEdit().setReadOnly(True)  # Prevent manual text input
+        self.combo_box.hide()
+
         # Confirm & Cancel Buttons (Hidden Initially)
         self.confirm_button = QPushButton("Confirm", self)
-        self.confirm_button.setGeometry(50, 130, 100, 30)
+        self.confirm_button.setGeometry(170, 130, 80, 30)
         self.confirm_button.clicked.connect(self.confirm)
         self.confirm_button.hide()
 
         self.cancel_button = QPushButton("Cancel", self)
-        self.cancel_button.setGeometry(180, 130, 100, 30)
+        self.cancel_button.setGeometry(240, 130, 80, 30)
         self.cancel_button.clicked.connect(self.reset_form)
         self.cancel_button.hide()
+
 
     def expand_form(self):
         """Expands the frame and shows the input fields"""
@@ -251,12 +290,24 @@ class CreateAccount(QWidget):
 
         # Show input fields and buttons
         self.expanded_bg_label.show()
+
+        # Brings entries to window
         self.riot_id_entry.show()
         self.tagline_entry.show()
         self.username_entry.show()
         self.password_entry.show()
+
+        # Clears the entries
+        self.riot_id_entry.clear()
+        self.tagline_entry.clear()
+        self.username_entry.clear()
+        self.password_entry.clear()
+
+        # Brings buttons to window
         self.confirm_button.show()
         self.cancel_button.show()
+
+        self.combo_box.show()
 
         # Force layout update and then scroll to the bottom
         self.updateGeometry()  # This ensures the layout is refreshed
@@ -280,6 +331,8 @@ class CreateAccount(QWidget):
         self.cancel_button.hide()
         self.expanded_bg_label.hide()
 
+        self.combo_box.hide()
+
         # Update the layout and adjust the parent window size
         self.parent().layout().update()  # Force the layout to refresh
         self.parent().adjustSize()  # Adjust the parent window size to fit the new layout
@@ -290,6 +343,7 @@ class CreateAccount(QWidget):
         tagline = self.tagline_entry.text()
         username = self.username_entry.text()
         password = self.password_entry.text()
+        region = self.combo_box.currentText()
 
         if not riot_id or not username or not password:
             print("Please fill all fields!")
@@ -297,16 +351,13 @@ class CreateAccount(QWidget):
 
         # Create a new AccountButton with the provided data
         new_account = AccountButton(
-            {"riot_id": riot_id, "tagline": tagline, "username": username, "password": password},
-            "bronze.png",  # Use a default image for now
-            w=random.randint(0, 1000),  # Random wins count for now
-            l=random.randint(0, 1000),  # Random losses count for now
+            {"riot_id": riot_id, "tagline": tagline, "region": region, "username": username, "password": password},
             width=400,  # Use a fixed width
             height=50,  # Use a fixed height
             radius=15  # Use a fixed radius
         )
 
-        users.append({"riot_id": riot_id, "tagline": tagline, "username": username, "password": password})
+        users.append({"riot_id": riot_id, "tagline": tagline, "region": region, "username": username, "password": password})
 
         # Get the layout of the parent widget
         parent_layout = self.parent().layout()
@@ -322,7 +373,7 @@ class CreateAccount(QWidget):
         if add_account_button_index != -1:
             # Insert the new account button before the "Add Account" button
             parent_layout.insertWidget(add_account_button_index, new_account)
-            
+
         # Delete Add account button if we have less than 6 users in the list
         if len(users) < 6:
             parent_layout.itemAt(add_account_button_index + 1).widget().deleteLater()
@@ -334,6 +385,8 @@ class CreateAccount(QWidget):
 class MainApp(QWidget):
     def __init__(self):
         super().__init__()
+        load_dotenv()
+
         self.setWindowTitle("Riot Logger")
         self.setGeometry(100, 100, 442, 372)  # x, y, w, h
 
@@ -364,9 +417,7 @@ class MainApp(QWidget):
         create_account_count = max_accounts_visible - len(users)
 
         for user in users:
-            w = random.randint(0, 1000)
-            l = random.randint(0, 1000)
-            scroll_layout.addWidget(AccountButton(user, "bronze.png", w, l, width, height, radius))
+            scroll_layout.addWidget(AccountButton(user, width, height, radius))
 
         # Add Create Account button
         for i in range(create_account_count):
